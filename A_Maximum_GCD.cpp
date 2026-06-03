8b57649962d26df0c10dd149b49479b48bef0560
towkir4601 <towkir4601@gmail.com>
2026-06-03 10:37:42 +0600

    Second Commit

--- A_Maximum_GCD.cpp
+++ A_Maximum_GCD.cpp
@@ -4,12 +4,9 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cout << "Enter a number: ";
     cin >> n;
     while(n--){
-        int t;
-        // cin>> t;
-        cout << "Hi Rafsan!!!"<< endl;
+     cout<< "Hello Towkir"<< endl;
     }
 
 return 0;
